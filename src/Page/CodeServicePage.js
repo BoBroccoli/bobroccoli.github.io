852be143e7a094ac2e176087d3801924ccdc54d3
BoBroccoli <wenxx194@d.umn.edu>
2023-03-11 23:46:15 -0500

    update

--- src/Page/CodeServicePage.js
+++ src/Page/CodeServicePage.js
@@ -1,18 +1,23 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const CodeServicePage = () => {
   return (
     <>
       <Stack spacing={2}>
+        <Box display='flex' flexDirection='row' justifyContent='center' alignItems='center'>
         <Box
           sx={{
             backgroundImage: 'url(./web_building.png)',
-            width: 350,
-            height: 350,
+            width: 650,
+            height: 650,
             backgroundSize: "cover",
           }}
         />
+        <Paper sx={{width: '30%', position: "relative", left: '-15%', backgroundColor: '#3AA14F'}}>
+          <Typography variant="h4" color={'white'}>Customize website for your portforlio or your business with much lower price.</Typography>
+        </Paper>
+        </Box>
       </Stack>
     </>
   );
