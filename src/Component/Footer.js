3696a5183b48c98020934d75c5a202c8c1a2297d
bwen <bwen@enova.com>
2023-03-24 14:06:24 -0400

    update

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,4 +1,4 @@
-import { Grid, Link, Typography } from "@mui/material";
+import { Grid, IconButton, Link, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -6,7 +6,7 @@ import EmailIcon from "@mui/icons-material/Email";
 const Footer = () => {
   return (
     <>
-      <Box sx={{display: 'flex', width: "100%", backgroundColor: "#585858" }}>
+      <Box sx={{ display: "flex", width: "100%", backgroundColor: "#585858" }}>
         <Grid container height="100%" padding={5}>
           <Grid item xs={12} md={4} padding={4}>
             <Box
@@ -16,11 +16,24 @@ const Footer = () => {
               justifyContent={"space-around"}
               height="100%"
             >
-              <Typography variant="h4" padding={2}>BoBrocoli</Typography>
-              <Typography variant="h7" padding={2}>Always want to be inspired</Typography>
-              <Box display={"flex"} justifyContent="space-around" width={100} padding={2}>
-                <InstagramIcon sx={{ color: "white" }} />
-                <EmailIcon sx={{ color: "white" }} />
+              <Typography variant="h4" padding={2}>
+                BoBrocoli
+              </Typography>
+              <Typography variant="h7" padding={2}>
+                Always want to be inspired
+              </Typography>
+              <Box
+                display={"flex"}
+                justifyContent="space-around"
+                width={100}
+                padding={2}
+              >
+                <IconButton href="https://www.instagram.com/bobroccoli_">
+                  <InstagramIcon sx={{ color: "white" }} />
+                </IconButton>
+                <IconButton href="mailto:bowen4091761@gmail.com">
+                  <EmailIcon sx={{ color: "white" }} />
+                </IconButton>
               </Box>
             </Box>
           </Grid>
@@ -35,7 +48,7 @@ const Footer = () => {
               <Link p={2} variant="h6" href="/">
                 {"My Services"}
               </Link>
-              <Link p={2} variant="h6" href="/">
+              <Link p={2} variant="h6" href="/about">
                 {"About Me"}
               </Link>
             </Box>
@@ -48,10 +61,10 @@ const Footer = () => {
               justifyContent={"flex-end"}
               height="70%"
             >
-              <Link p={2} variant="h6" href="/">
+              <Link p={2} variant="h6" href="/about">
                 {"Contact"}
               </Link>
-              <Link p={2} variant="h6" href="/">
+              <Link p={2} variant="h6" href="/schedule">
                 {"Schedule"}
               </Link>
             </Box>
